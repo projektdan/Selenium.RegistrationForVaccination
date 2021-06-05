b813f34ccc577417dbe39ca8c2fea912ebc2bc82
Daniel Walowski <daniel.walowski@apator.com>
2021-06-05 16:39:13 +0200

    #5 Create app layer, add services configuration

---
+++ Selenium.RegistrationForVaccination.ConsoleUI/Application.cs
@@ -0,0 +1,19 @@
+﻿using Selenium.RegistrationForVaccination.Infrastructure;
+
+namespace Selenium.RegistrationForVaccination.ConsoleUI
+{
+    public class Application
+    {
+        private readonly IRegisterForVisit registerForVisit;
+
+        public Application(IRegisterForVisit registerForVisit)
+        {
+            this.registerForVisit = registerForVisit;
+        }
+
+        public void Run()
+        {
+            registerForVisit.Register();
+        }
+    }
+}
--- Selenium.RegistrationForVaccination.ConsoleUI/Program.cs
+++ Selenium.RegistrationForVaccination.ConsoleUI/Program.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Selenium.RegistrationForVaccination.Infrastructure.Extensions;
+using System.IO;
 
 namespace Selenium.RegistrationForVaccination.ConsoleUI
 {
@@ -6,7 +9,33 @@ namespace Selenium.RegistrationForVaccination.ConsoleUI
     {
         static void Main(string[] args)
         {
-            
+            var services = ConfigureServices();
+            var serviceProvider = services.BuildServiceProvider();
+
+            serviceProvider.GetService<Application>().Run();
+        }
+
+        private static IServiceCollection ConfigureServices()
+        {
+            var services = new ServiceCollection();
+            var configuration = LoadConfiguration();
+
+            services.AddSingleton(configuration);
+            services.AddSingleton<Application>();
+            services.RegisterModules();
+            services.AddCredentialsOptions();
+            services.AddRegistrationOptions();
+
+            return services;
+        }
+
+        private static IConfiguration LoadConfiguration()
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true);
+
+            return builder.Build();
         }
     }
 }
