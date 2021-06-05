44f26e4e91312abe24c7f7a44cfbde77dd3ed50b
Daniel Walowski <daniel.walowski@apator.com>
2021-06-05 16:33:21 +0200

    #4 Create unit tests

---
+++ Selenium.RegistrationForVaccination.Infrastructure.Tests/Extensions/FlowsExtensionsTests.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Selenium.RegistrationForVaccination.Infrastructure.Extensions;
+using Selenium.RegistrationForVaccination.Infrastructure.Repositories;
+
+namespace Selenium.RegistrationForVaccination.Infrastructure.Tests.Extensions
+{
+    [TestClass]
+    public class FlowsExtensionsTests
+    {
+        [TestMethod]
+        public void IsDateInRange_OfDateDeclaredArray_ShouldReturnTrue()
+        {
+            InMemoryBookingDatesRepository datesRepository = new InMemoryBookingDatesRepository();
+            var mockDataFromPage = "Termin wizyty 25 maja 2021 10:30";
+            var matchingVisitDates = datesRepository.Get();
+
+            Assert.IsTrue(mockDataFromPage.IsDateInRangeOf(matchingVisitDates));
+        }
+
+        [TestMethod]
+        public void IsLocationInRange_OfLocationDeclaredArray_ShouldReturnTrue()
+        {
+            InMemoryBookingLocationsRepository locationsRepository = new InMemoryBookingLocationsRepository();
+            var mockDataFromPage = "Lokalizacja wizyty: SZPITAL TYMCZASOWY MTP " +
+                "w punkcie Heliodora Swiecickiego brama nr 4";
+            var matchingVisitLocations = locationsRepository.Get();
+
+            Assert.IsTrue(mockDataFromPage.IsLocationInRangeOf(matchingVisitLocations));
+        }
+    }
+}
